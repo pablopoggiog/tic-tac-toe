7a019cbc8f7df67733032934099abcf606c79ddc
Pablo Poggio <pablopoggiog@gmail.com>
2021-01-28 23:31:51 -0300

    Create useGame hook and move all the game logic there

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { Board, Toggler, SocialMedia, Animation } from "..";
-import { SquaresState, Turn, initialState, checkWinner } from "../../lib";
+import { useGame } from "../../hooks";
 import { Container, Button } from "./styles";
 
 interface GameProps {
@@ -8,75 +8,7 @@ interface GameProps {
 }
 
 export const Game: FunctionComponent<GameProps> = ({ toggleTheme }) => {
-  const [turn, setTurn] = useState<number>(0);
-
-  const [winner, setWinner] = useState<Turn>(null);
-
-  const [animation, setAnimation] = useState<string>("");
-
-  const [squares, setSquares] = useState<SquaresState>(
-    JSON.parse(JSON.stringify(initialState)) // i have to use these 2 JSON methods to pass a full new objet as initial state,
-  ); // otherwise because of the nesting levels it'll have weird behaviours muting initialState const value during the app usage
-
-  const playSquare = (index: number) => {
-    if (!squares[index].selected) {
-      setTurn(turn + 1);
-
-      const newSquares = { ...squares };
-
-      newSquares[index].flip = true;
-
-      newSquares[index].selected = turn % 2 ? "X" : "O";
-
-      setSquares({ ...newSquares });
-
-      setTimeout(() => {
-        newSquares[index].flip = false;
-
-        setSquares({ ...newSquares });
-      }, 2000);
-    }
-  };
-
-  const handleClick = (index: number) => {
-    !winner && playSquare(index);
-
-    const someoneWon = checkWinner(squares);
-
-    if (someoneWon) {
-      setWinner(someoneWon);
-
-      setAnimation("winner");
-
-      setTimeout(() => {
-        setAnimation("");
-      }, 4500);
-    }
-
-    if (turn === 9) {
-      setAnimation("tie");
-
-      setTimeout(() => {
-        setAnimation("");
-      }, 4500);
-    }
-  };
-
-  const resetGame = () => {
-    setAnimation("reset");
-
-    setTimeout(() => {
-      setAnimation("");
-    }, 4500);
-
-    setWinner(null);
-
-    setTurn(0);
-
-    setTimeout(() => {
-      setSquares(JSON.parse(JSON.stringify(initialState)));
-    }, 2200);
-  };
+  const { animation, winner, squares, handleClick, resetGame } = useGame();
 
   return (
     <Container>
---
+++ src/hooks/useGame/index.ts
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { SquaresState, Turn, initialState, checkWinner } from "../../lib";
+
+export const useGame = () => {
+  const [turn, setTurn] = useState<number>(0);
+
+  const [winner, setWinner] = useState<Turn>(null);
+
+  const [animation, setAnimation] = useState<string>("");
+
+  const [squares, setSquares] = useState<SquaresState>(
+    JSON.parse(JSON.stringify(initialState)) // i have to use these 2 JSON methods to pass a full new objet as initial state,
+  ); // otherwise because of the nesting levels it'll have weird behaviours muting initialState const value during the app usage
+
+  const playSquare = (index: number) => {
+    if (!squares[index].selected) {
+      setTurn(turn + 1);
+
+      const newSquares = { ...squares };
+
+      newSquares[index].flip = true;
+
+      newSquares[index].selected = turn % 2 ? "X" : "O";
+
+      setSquares({ ...newSquares });
+
+      setTimeout(() => {
+        newSquares[index].flip = false;
+
+        setSquares({ ...newSquares });
+      }, 2000);
+    }
+  };
+
+  const handleClick = (index: number) => {
+    !winner && playSquare(index);
+
+    const someoneWon = checkWinner(squares);
+
+    if (someoneWon) {
+      setWinner(someoneWon);
+
+      setAnimation("winner");
+
+      setTimeout(() => {
+        setAnimation("");
+      }, 4500);
+    }
+
+    if (turn === 9) {
+      setAnimation("tie");
+
+      setTimeout(() => {
+        setAnimation("");
+      }, 4500);
+    }
+  };
+
+  const resetGame = () => {
+    setAnimation("reset");
+
+    setTimeout(() => {
+      setAnimation("");
+    }, 4500);
+
+    setWinner(null);
+
+    setTurn(0);
+
+    setTimeout(() => {
+      setSquares(JSON.parse(JSON.stringify(initialState)));
+    }, 2200);
+  };
+
+  return {
+    animation,
+    winner,
+    squares,
+    handleClick,
+    resetGame,
+  };
+};
